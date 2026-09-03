7807ebce7de784dba6dd9ea5adfdfcebb799d680
rohanramesh15 <rohanramesh15@gmail.com>
2026-09-03 11:20:04 -0700

    Recover stalled YouTube caption capture

--- src/components/ExtensionInstallModal.tsx
+++ src/components/ExtensionInstallModal.tsx
@@ -4,7 +4,7 @@ import { Download, Puzzle, X } from 'lucide-react';
 import { useDialogFocus } from '../hooks/useDialogFocus';
 import { Button } from './ui/button';
 
-const EXTENSION_DOWNLOAD_URL = '/downloads/clipit-extension-1.5.12.zip';
+const EXTENSION_DOWNLOAD_URL = '/downloads/clipit-extension-1.5.13.zip';
 
 interface ExtensionInstallContextValue {
   openExtensionInstall: () => void;
